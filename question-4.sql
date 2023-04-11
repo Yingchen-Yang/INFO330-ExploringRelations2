2f2e80bc3b9fdf714494bcec835ad92851810802
Yingchen-Yang <yingcy2@uw.edu>
2023-04-11 14:38:57 -0700

    question4

--- question-4.sql
+++ question-4.sql
@@ -1,1 +1,10 @@
 -- Which sales agent made the most in sales in 2010?
+SELECT employees.FirstName, employees.LastName, 
+       SUM(invoices.Total) AS total_sales
+FROM employees
+JOIN customers ON employees.EmployeeId = customers.SupportRepId
+JOIN invoices ON customers.CustomerId = invoices.CustomerId
+WHERE strftime ('%Y', invoices.InvoiceDate) =  '2010'
+GROUP BY employees.EmployeeId
+ORDER BY total_sales DESC
+LIMIT 1;
