cfa93f4781897be01cbc8f0fa365bbb51e7f2e95
Yingchen-Yang <yingcy2@uw.edu>
2023-04-11 14:40:42 -0700

    question5

--- question-5.sql
+++ question-5.sql
@@ -1,1 +1,8 @@
 -- What was the most purchased track of 2013?
+SELECT tracks.Name, COUNT(*) AS total_purchase
+FROM tracks
+JOIN invoice_items ON tracks.TrackId = invoice_items.TrackId                                                                                                                                             
+JOIN invoices ON invoice_items.InvoiceId = invoices.InvoiceId
+WHERE strftime('%Y', invoices.InvoiceDate) like '2013%'
+GROUP BY tracks.TrackId
+ORDER BY total_purchase DESC;
