af4540144bb661004402482de88070a29d423234
Yingchen-Yang <yingcy2@uw.edu>
2023-04-11 14:37:49 -0700

    question2

--- question-2.sql
+++ question-2.sql
@@ -1,1 +1,5 @@
 -- Show the sales agent's full name and invoices associated with each sales agent.
+SELECT employees.FirstName, employees.LastName, invoices.*
+FROM employees
+INNER JOIN customers ON employees.EmployeeId = customers.SupportRepId
+INNER JOIN invoices ON customers.CustomerId = invoices.CustomerId;
